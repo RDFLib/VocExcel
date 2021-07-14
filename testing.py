e5c825d8d8a72bbbcc175a05a8a762f433a922aa
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-07-14 14:01:19 +1000

    demo rdf format parse

--- testing.py
+++ testing.py
@@ -2,3 +2,8 @@ from vocexcel import convert
 
 # convert.rdf_to_excel("vocab-invalid.ttl")
 print(convert.excel_to_rdf("eg-valid.xlsx", output_type="string", output_format="xml"))
+
+from rdflib import Graph
+
+g = Graph().parse("tests/eg-rdf-valid.xml")
+print(len(g))
