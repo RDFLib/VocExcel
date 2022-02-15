884caa3b4f47f4cb9ffe74b5cce4c0beb1e813f4
peterphilips <peter.philips@surroundaustralia.com>
2022-02-15 15:36:44 +1100

    added logic in functions to do different things depending on template version

--- vocexcel/convert.py
+++ vocexcel/convert.py
@@ -39,7 +39,7 @@ def split_and_tidy(cell_value: str):
 
 
 def extract_concepts_and_collections(
-    s: Worksheet, a: Worksheet,
+    s: Worksheet, a: Worksheet, b: Worksheet
 ) -> Tuple[List[models.Concept], List[models.Collection]]:
     concepts = []
     collections = []
@@ -48,92 +48,98 @@ def extract_concepts_and_collections(
     for col in s.iter_cols(max_col=1):
         for cell in col:
             row = cell.row
+            global template_version
             if template_version == "0.4.0":
+                c = models.Concept(
+                    uri=s[f"A{row}"].value,
+                    pref_label=s[f"B{row}"].value,
+                    pl_language_code=split_and_tidy(s[f"C{row}"].value),  # new in 0.4.0
+                    alt_labels=split_and_tidy(s[f"D{row}"].value),
+                    definition=s[f"E{row}"].value,
+                    def_language_code=split_and_tidy(s[f"F{row}"].value),  # new in 0.4.0
+                    children=split_and_tidy(s[f"G{row}"].value),
+                    other_ids=split_and_tidy(s[f"H{row}"].value),
+                    home_vocab_uri=s[f"I{row}"].value,
+                    provenance=s[f"J{row}"].value,
+                    related_match=a[f"B{row}"].value,
+                    close_match=a[f"C{row}"].value,
+                    exact_match=a[f"D{row}"].value,
+                    narrow_match=a[f"E{row}"].value,
+                    broad_match=a[f"F{row}"].value,
+                )
+                col = models.Collection(
+                    uri=b[f"A{row}"].value,
+                    pref_label=b[f"B{row}"].value,
+                    definition=b[f"C{row}"].value,
+                    members=split_and_tidy(b[f"D{row}"].value),
+                    provenance=b[f"E{row}"].value,
+                )
+            concepts.append(c)
+            collections.append(col)
+        else:
+            if cell.value == "Concept URI":
+                process_concept = True
+            elif cell.value == "Collection URI":
+                process_concept = False
+                process_collection = True
+            elif process_concept:
                 if cell.value is None:
                     pass
                 else:
-                    c = models.Concept(
-                        uri=s[f"A{row}"].value,
-                        pref_label=s[f"B{row}"].value,
-                        pl_language_code=split_and_tidy(s[f"C{row}"].value),  # new in 0.4.0
-                        alt_labels=split_and_tidy(s[f"D{row}"].value),
-                        definition=s[f"E{row}"].value,
-                        def_language_code=split_and_tidy(s[f"F{row}"].value),  # new in 0.4.0
-                        children=split_and_tidy(s[f"G{row}"].value),
-                        other_ids=split_and_tidy(s[f"H{row}"].value),
-                        home_vocab_uri=s[f"I{row}"].value,
-                        provenance=s[f"J{row}"].value,
-                        related_match=a[f"B{row}"].value,
-                        close_match=a[f"C{row}"].value,
-                        exact_match=a[f"D{row}"].value,
-                        narrow_match=a[f"E{row}"].value,
-                        broad_match=a[f"F{row}"].value,
-                    )
-                concepts.append(c)
-            else:
-                if cell.value == "Concept URI":
-                    process_concept = True
-                elif cell.value == "Collection URI":
-                    process_concept = False
-                    process_collection = True
-                elif process_concept:
-                    if cell.value is None:
-                        pass
-                    else:
-                        try:
-                            global template_version
-                            if template_version == "0.3.0":
-                                c = models.Concept(
-                                    uri=s[f"A{row}"].value,
-                                    pref_label=s[f"B{row}"].value,
-                                    pl_language_code=split_and_tidy(s[f"C{row}"].value),  # new in 0.3.0
-                                    alt_labels=split_and_tidy(s[f"D{row}"].value),
-                                    definition=s[f"E{row}"].value,
-                                    def_language_code=split_and_tidy(s[f"F{row}"].value),  # new in 0.3.0
-                                    children=split_and_tidy(s[f"G{row}"].value),
-                                    other_ids=split_and_tidy(s[f"H{row}"].value),
-                                    home_vocab_uri=s[f"I{row}"].value,
-                                    provenance=s[f"J{row}"].value,
-                                    template_version=template_version
-                                )
-                            elif template_version == "0.2.1":
-                                c = models.Concept(
-                                    uri=s[f"A{row}"].value,
-                                    pref_label=s[f"B{row}"].value,
-                                    alt_labels=split_and_tidy(s[f"C{row}"].value),
-                                    definition=s[f"D{row}"].value,
-                                    children=split_and_tidy(s[f"E{row}"].value),
-                                    other_ids=split_and_tidy(s[f"F{row}"].value),
-                                    home_vocab_uri=s[f"G{row}"].value,
-                                    provenance=s[f"H{row}"].value,
-                                )
-                            else:
-                                raise ConversionError("The version of the Excel template your are using is not recognised")
-
-                            concepts.append(c)
-                        except ValidationError as e:
-                            raise ConversionError(
-                                f"Concept processing error, row {row}, error: {e}"
-                            )
-                elif process_collection:
-                    if cell.value is None:
-                        pass
-                    else:
-                        try:
-                            c = models.Collection(
+                    try:
+                        global template_version
+                        if template_version == "0.3.0":
+                            c = models.Concept(
                                 uri=s[f"A{row}"].value,
                                 pref_label=s[f"B{row}"].value,
-                                definition=s[f"C{row}"].value,
-                                members=split_and_tidy(s[f"D{row}"].value),
-                                provenance=s[f"E{row}"].value,
+                                pl_language_code=split_and_tidy(s[f"C{row}"].value),  # new in 0.3.0
+                                alt_labels=split_and_tidy(s[f"D{row}"].value),
+                                definition=s[f"E{row}"].value,
+                                def_language_code=split_and_tidy(s[f"F{row}"].value),  # new in 0.3.0
+                                children=split_and_tidy(s[f"G{row}"].value),
+                                other_ids=split_and_tidy(s[f"H{row}"].value),
+                                home_vocab_uri=s[f"I{row}"].value,
+                                provenance=s[f"J{row}"].value,
+                                template_version=template_version
                             )
-                            collections.append(c)
-                        except ValidationError as e:
-                            raise ConversionError(
-                                f"Collection processing error, row {row}, error: {e}"
+                        elif template_version == "0.2.1":
+                            c = models.Concept(
+                                uri=s[f"A{row}"].value,
+                                pref_label=s[f"B{row}"].value,
+                                alt_labels=split_and_tidy(s[f"C{row}"].value),
+                                definition=s[f"D{row}"].value,
+                                children=split_and_tidy(s[f"E{row}"].value),
+                                other_ids=split_and_tidy(s[f"F{row}"].value),
+                                home_vocab_uri=s[f"G{row}"].value,
+                                provenance=s[f"H{row}"].value,
                             )
-                elif cell.value is None:
+                        else:
+                            raise ConversionError("The version of the Excel template your are using is not recognised")
+
+                        concepts.append(c)
+                    except ValidationError as e:
+                        raise ConversionError(
+                            f"Concept processing error, row {row}, error: {e}"
+                        )
+            elif process_collection:
+                if cell.value is None:
                     pass
+                else:
+                    try:
+                        c = models.Collection(
+                            uri=s[f"A{row}"].value,
+                            pref_label=s[f"B{row}"].value,
+                            definition=s[f"C{row}"].value,
+                            members=split_and_tidy(s[f"D{row}"].value),
+                            provenance=s[f"E{row}"].value,
+                        )
+                        collections.append(c)
+                    except ValidationError as e:
+                        raise ConversionError(
+                            f"Collection processing error, row {row}, error: {e}"
+                        )
+            elif cell.value is None:
+                pass
 
     return concepts, collections
 
@@ -151,11 +157,21 @@ def excel_to_rdf(
     if not file_to_convert_path.name.endswith(tuple(EXCEL_FILE_ENDINGS)):
         raise ValueError("Files for conversion to RDF must be Excel files ending .xlsx")
     wb = load_workbook(filename=str(file_to_convert_path), data_only=True)
-    pi = wb["program info"]
-    global template_version
-    template_version = pi["B2"].value
-
-    sheet = wb["vocabulary" if sheet_name is None else sheet_name]
+    try:
+        pi = wb["program info"]
+        global template_version
+        template_version = pi["B2"].value
+        sheet = wb["vocabulary" if sheet_name is None else sheet_name]
+        concepts, collections = extract_concepts_and_collections(sheet)
+    except NameError:
+        intro_sheet = wb["Introduction"]
+        global template_version
+        template_version = intro_sheet["J11"].value
+        sheet = wb["Concept Scheme"]
+        concept_sheet = wb["Concepts"]
+        additional_concept_sheet = wb["Additional Concept Features"]
+        collection_sheet = wb["Collection"]
+        concepts, collections = extract_concepts_and_collections(concept_sheet, additional_concept_sheet, collection_sheet)
 
     # Vocabulary
     try:
@@ -174,9 +190,6 @@ def excel_to_rdf(
         )
     except ValidationError as e:
         raise ConversionError(f"ConceptScheme processing error: {e}")
-
-    # Concepts & Collections
-    concepts, collections = extract_concepts_and_collections(sheet)
 
     # Build the total vocab
     v = models.Vocabulary(concept_scheme=cs, concepts=concepts, collections=collections)
