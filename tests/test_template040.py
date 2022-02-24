b327c460086f9151acda1a9801e3720844ba955a
nicholascar <nicholas.car@surroundaustralia.com>
2022-02-24 16:19:11 +1000

    blacked file

--- tests/test_template040.py
+++ tests/test_template040.py
@@ -9,12 +9,13 @@ from vocexcel import convert
 def test_baddata_poor_additional_concepts_example():
     with pytest.raises(Exception) as exc_info:
         convert.excel_to_rdf(
-                Path(__file__).parent / "040_baddata.xlsx",
-                output_type="file",)
+            Path(__file__).parent / "040_baddata.xlsx",
+            output_type="file",
+        )
 
 
 def test_simple():
     convert.excel_to_rdf(
-            Path(__file__).parent / "040_baddata.xlsx",
-            output_type="file",)
-
+        Path(__file__).parent / "040_baddata.xlsx",
+        output_type="file",
+    )
