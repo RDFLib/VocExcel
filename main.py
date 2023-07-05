9904958ddd3e05ce34eb61eea9f81ff56b233056
Edmond Chuc <edmond@kurrawong.ai>
2023-07-05 15:39:01 +1000

    Remove webbrowser

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import webbrowser
-
 import uvicorn
 
 
 if __name__ == "__main__":
-    webbrowser.open("http://localhost:8000", new=2)
     uvicorn.run("vocexcel.web.app:create_app", host="0.0.0.0", port=8000, reload=True)
