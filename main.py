f189464ec9dbbf56825c9be75472b353f84514d1
Edmond Chuc <edmond@kurrawong.ai>
2023-07-09 18:23:28 +1000

    Add uvicorn factory flag to true

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("vocexcel.web.app:create_app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "vocexcel.web.app:create_app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        factory=True,
+    )
